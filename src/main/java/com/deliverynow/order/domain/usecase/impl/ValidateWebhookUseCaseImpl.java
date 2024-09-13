2cd52f081d7ba2cae390c9c125e930dae24aaa03
coelho566 <45610323+coelho566@users.noreply.github.com>
2024-09-13 16:51:10 -0300

    Ajustes configuração processo de pagamento

--- src/main/java/com/deliverynow/order/domain/usecase/impl/ValidateWebhookUseCaseImpl.java
+++ src/main/java/com/deliverynow/order/domain/usecase/impl/ValidateWebhookUseCaseImpl.java
@@ -5,9 +5,11 @@ import com.deliverynow.order.domain.usecase.ValidateWebhookUseCase;
 import com.deliverynow.order.infrastructure.config.OrderProperties;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
 
+@Slf4j
 public class ValidateWebhookUseCaseImpl implements ValidateWebhookUseCase {
 
     OrderProperties orderProperties;
@@ -18,7 +20,7 @@ public class ValidateWebhookUseCaseImpl implements ValidateWebhookUseCase {
 
     @Override
     public Optional<String> getPaymentIdOrder(String payload) {
-
+        log.info("Iniciando validação do payload de pagamento {}", payload);
         var webhookPayload = getWebHookPayload(payload);
         if (webhookPayload != null && isMerchantOrder(webhookPayload)) {
             var orderId = webhookPayload.getResource().substring(webhookPayload.getResource().lastIndexOf('/') + 1);
